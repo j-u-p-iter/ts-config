a9c45df051e6465c9a3a533d4625d2a2d32b62d1
Kirill <pn79122821224@gmail.com>
2021-01-04 16:29:41 +0500

    fix(uses this.configPath instead of this.name and adds some docs):

--- src/TSConfig.ts
+++ src/TSConfig.ts
@@ -5,19 +5,39 @@ import { readFileSync } from "fs";
 import path from "path";
 import { readConfigFile } from "typescript";
 
+/**
+ * Sometimes we need to register the TypeScript compiler programmatically
+ *   with "tsNode.register(tsConfig)".
+ *
+ *   E.g. we do like this:
+ *     - in the custom repl;
+ *     - in the test runner.
+ *
+ * For such cases we need to read the TypeScript config by ourself.
+ *
+ * This class serves exactly for this purpose.
+ *
+ * What it does is:
+ *   - reads the TypeScript config, using the TS API (readConfigFile method).
+ *   - caches TS config until it's content or location are changed.
+ *
+ */
 export class TSConfig {
+  private configPath = null;
+
   private cacheFolderPath = null;
 
-  private configPath = null;
   /**
-   * We read the config to use the content in the InFilesCache
+   * We read the config's raw content to use the content in the InFilesCache.
+   *   The presence of the config is the mandatory requirement. So, if there's no
+   *   the config in the application we throw an appropriate error.
    *
    */
   private async getConfigRawContent(): Promise<string | null> {
     const pathToConfig = await this.resolvePathToConfig();
 
     try {
-      const fileContent = readFileSync(pathToConfig, "utf8");
+      const fileContent = this.readFile(pathToConfig);
 
       return fileContent;
     } catch (error) {
@@ -32,17 +52,20 @@ export class TSConfig {
   }
 
   /**
-   * We cache parsed tsconfig result not to compile
-   *   it everytime we need it.
+   * We cache parsed tsconfig result not to parse
+   *   it everytime we use it.
+   *   The cache will be updated as soon as the config's content
+   *   or location are changed.
    *
    */
   private cache: InFilesCache | null = null;
 
   /**
-   * Config name should always be called "tsconfig.json"
+   * Default path to the config. You can reassign it with
+   *   the "configPath" option.
    *
    */
-  private name: string = "tsconfig.json";
+  private defaultPathToConfig: string = "./tsconfig.json";
 
   /**
    * Detects the root path to the project by location of
@@ -55,35 +78,72 @@ export class TSConfig {
     return dirPath;
   }
 
+  /**
+   * Initialise cache instance. We use it to store parsed
+   *   config result.
+   *
+   */
   private async initCache() {
     if (!this.cache) {
       this.cache = new InFilesCache(this.cacheFolderPath);
     }
   }
 
+  /**
+   * Calculate cache params, that are used to get/set the cache.
+   *
+   */
   private async getCacheParams(): Promise<CacheParams> {
     const appRootFolderPath = await this.getAppRootFolderPath();
     const configRawContent = await this.getConfigRawContent();
+    const resolvedPathToConfig = await this.resolvePathToConfig();
 
     return {
-      filePath: path.resolve(appRootFolderPath, this.name),
+      filePath: path.resolve(appRootFolderPath, resolvedPathToConfig),
       fileContent: configRawContent,
       fileExtension: ".json"
     };
   }
 
+  /**
+   * The config's path should be relative to the app's root folder.
+   *   Here we create the absolute path to the config to make it
+   *   univeral and independent from the file's location, that uses
+   *   this path.
+   *
+   */
   private async resolvePathToConfig() {
     const appRootFolderPath = await this.getAppRootFolderPath();
 
     return path.resolve(appRootFolderPath, this.configPath);
   }
 
+  /**
+   * We need to converth the output into the String,
+   *   becuase readConfigFile expects the method, that returns the String.
+   *   Otherwise the TS compiltion error will be emitted.
+   *
+   */
+  private readFile(pathToConfig) {
+    return readFileSync(pathToConfig).toString();
+  }
+
+  /**
+   * We read the config file, using TS API (readConfigFile).
+   *   It validates the config and:
+   *   - if it's valid - returns parsed config;
+   *   - if it's not valid - returns an error.
+   *
+   *   Internally this method is pretty big (because of multiple validation steps),
+   *   so, it's better to cache it.
+   *
+   */
   private async readConfig() {
     const resolvedPathToConfig = await this.resolvePathToConfig();
 
     const { error, config } = readConfigFile(
       resolvedPathToConfig,
-      readFileSync
+      this.readFile
     );
 
     if (error) {
@@ -96,10 +156,14 @@ export class TSConfig {
   }
 
   constructor(options: { configPath?: string; cacheFolderPath: string }) {
-    this.configPath = options.configPath;
+    this.configPath = options.configPath || this.defaultPathToConfig;
     this.cacheFolderPath = options.cacheFolderPath;
   }
 
+  /**
+   * Parses TS config
+   *
+   */
   public async parse() {
     await this.initCache();
 
